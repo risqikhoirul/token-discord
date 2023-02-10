d411160b33a9de15ed8448b5c9a6fc5c5793ad23
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-02-10 10:37:04 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 import DiscordDatabase from "discord-cloud-database";
 const discordDatabase = new DiscordDatabase();
 import fs from "fs";
+import path from "path";
 /**
  * M Khoirul Risqi
  * https://github.com/risqikhoirul
@@ -8,6 +9,16 @@ import fs from "fs";
  */
 
 (async () => {
+  const modulesToCheck = ["discord-cloud-database"];
+
+  for (const moduleName of modulesToCheck) {
+    const modulePath = path.join(__dirname, "node_modules", moduleName);
+
+    if (!fs.existsSync(modulePath)) {
+      console.error(`Error: ${moduleName} is not installed. Please run "npm install ${moduleName}" and try again.`);
+      process.exit(1);
+    }
+  }
   const email = process.argv[2];
   const password = process.argv[3];
   try {
@@ -25,6 +36,10 @@ import fs from "fs";
       console.log("\ntoken telah tersimpan di outoken.txt!");
     });
   } catch (error) {
-    throw error;
+    if (error.error.discordError === true) {
+      console.log("please check email and password");
+    } else {
+      console.error(error);
+    }
   }
 })();
